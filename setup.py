bd54c7afb7521a7a1ce6bccfb1ff7e2c266d1406
Ricardo <ricardo.gm@live.com>
2023-09-02 21:40:51 -0700

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='neutrino-cli',
-    version='0.1.8',
+    version='0.1.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
